eab85a0d9fdcde41bb1691e0ca45a1176f37dc3b
test <test@briup.com>
2016-10-19 13:59:04 +0800

    JavaScript Programs

---
+++ apple-tree/js/position.js
@@ -0,0 +1,16 @@
+var position = [{
+	top:"340px",
+	left:"110px"
+},{
+	top:"380px",
+	left:"110px"
+},{
+	top:"390px",
+	left:"210px"
+},{
+	top:"410px",
+	left:"340px"
+},{
+	top:"420px",
+	left:"450px"
+}]
